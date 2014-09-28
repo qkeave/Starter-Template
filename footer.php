538b44cbc851de24d35dc15c67956a100e794622
qkeave <qkeave@gmail.com>
2014-09-28 18:55:19 -0500

    cleaner

--- footer.php
+++ footer.php
@@ -1,25 +1,13 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package Crucial Detail
- */
-?>
 
-	</div><!-- #content -->
+<footer id="colophon" class="site-footer container" role="contentinfo">
+	<section class="footer">
 
-	<footer id="colophon" class="site-footer container" role="contentinfo">
-		<section class="footer">
-
-		</section>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	</section>
+</footer><!-- #colophon -->
 
 <!-- Javascript -->
 <!-- In the footer for better performance -->
-<script src="<?php echo get_bloginfo('template_directory');?>/build/js/scripts.js"></script>
+<script src="build/js/scripts.js"></script>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <!-- (Via HTML5 Boilerplate: http://html5boilerplate.com/) -->
@@ -29,8 +17,6 @@
 	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
-
-<?php wp_footer(); ?>
 
 </body>
 </html>
